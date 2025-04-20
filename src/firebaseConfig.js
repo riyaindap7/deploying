28814a9310d2d22d5a47028cd12ca896faf7afb8
Riya <riyaindap2000@gmail.com>
2025-04-21 01:58:29 +0530

    fifth commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,24 +1,25 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import 'firebase/database'; 
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-  };
+  apiKey: "AIzaSyDAV2FRysXeGeyX98oweUPDSTks2n7hMfM",
+  authDomain: "shrutidemo-2e752.firebaseapp.com",
+  projectId: "shrutidemo-2e752",
+  storageBucket: "shrutidemo-2e752.appspot.com",
+  messagingSenderId: "354089387287",
+  appId: "1:354089387287:web:579fca4e654d62d005a352",
+  measurementId: "G-6M6148RZ70"
+};
 
-// Initialize Firebase app and services
+// Initialize Firebase app and authentication
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app); // Authentication
-const db = getFirestore(app); // Firestore database
-const storage = getStorage(app); // Storage service
-const googleProvider = new GoogleAuthProvider(); // Google Auth Provider
+const auth = getAuth(app); // For authentication
+const db = getFirestore(app);
+const storage = getStorage(app);
+const googleProvider = new GoogleAuthProvider();
 
-export { auth, storage, googleProvider };
+export { auth, storage , googleProvider};
 export default db;
